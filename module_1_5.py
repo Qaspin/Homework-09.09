2953e988ec3661712a1ab74a5a89983c3d9cd284
Qaspin <1117-zrp@mail.ru>
2024-09-09 19:43:37 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1 , 2 , "строка" , False
+print(immutable_var)
+# immutable_var [0:2] = [3, 4]  - кортеж не изменяемый тип элементов
+mutable_list = [1 , 2 , "строка" , False]
+mutable_list.append("университет")
+print(mutable_list)
+
